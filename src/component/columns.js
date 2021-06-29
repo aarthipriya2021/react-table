5b8c56fa70e3e6c77019c646a115fc6a57ff615f
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-06-29 23:19:52 +0530

    Sorting the date

--- src/component/columns.js
+++ src/component/columns.js
@@ -1,3 +1,4 @@
+import { format} from 'date-fns';
 export const COLUMNS = [
     {
         Header: 'Id',
@@ -18,6 +19,7 @@ export const COLUMNS = [
         Header: 'Date of Birth',
         Footer: 'Date of Birth',
         accessor: 'date_of_birth',
+        Cell: ({ value}) => { return format(new Date(value), 'dd/MM/yyyy')}
     },
     {
         Header: 'Country',
@@ -61,6 +63,7 @@ export const GROUPED_COLUMNS = [
                 Header: 'Date of Birth',
                 Footer: 'Date of Birth',
                 accessor: 'date_of_birth',
+                Cell: ({ value}) => { return format(new Date(value), 'dd/MM/yyyy')}
             },
             {
                 Header: 'Country',
