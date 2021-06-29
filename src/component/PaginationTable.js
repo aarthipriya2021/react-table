6d0734bbfd0466e160c04ab535091eba56690e00
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-06-30 02:26:23 +0530

    Pagination(page size)

--- src/component/PaginationTable.js
+++ src/component/PaginationTable.js
@@ -28,6 +28,8 @@ export const  PaginationTable = () => {
         canPreviousPage,
         pageOptions,
         state,
+        gotoPage,
+        pageCount,
     } = tableInstance
 const { pageIndex} = state;
 
@@ -70,8 +72,21 @@ const { pageIndex} = state;
                         {pageIndex + 1} of {pageOptions.length}
                     </strong>{' '}
                 </span>
+                <span>
+                    | Go to page : {' '}
+                    <input type='number'defaultValue={pageIndex + 1} 
+                    onChange={(e) => {
+                        const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                        gotoPage(pageNumber)
+                    }}
+                    style={{width:'50px'}}
+                    />
+
+                </span>
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
                 <button onClick= {()=> previousPage()} disabled={!canPreviousPage}>Previous</button>
                 <button onClick= {()=> nextPage()} disabled={!canNextPage}>Next</button>
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
             </div>
         </>
         
