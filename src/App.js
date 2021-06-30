e19bad793056831cbc15151baff0ffffe73fb072
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-07-01 00:09:45 +0530

    Column Ordering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 
+
 // import BasicTable from './component/BasicTable';
 // import SortingTable from './component/SortingTable';
 // import { FilteringTable} from './component/FilteringTable';
 // import { PaginationTable} from './component/PaginationTable';
-import { RowSelection} from './component/RowSelection'; 
+// import { RowSelection} from './component/RowSelection'; 
+import { ColumnOrder } from './component/ColumnOrder';
+
 function App() {
   return (
-    <div >
+    <div className="App">
       {/* <BasicTable /> */}
         {/* <SortingTable /> */}
           {/* <FilteringTable /> */}
             {/* <PaginationTable /> */}
-              <RowSelection />
+              {/* <RowSelection /> */}
+                <ColumnOrder />
     </div>
   );
 }
